39d177d783f5195169bc12de6bbd2fc77ae8136a
박성훈 / Koder <koder0205@gmail.com>
2020-08-03 10:14:29 +0900

    update
    아마도 최종업데이트

--- Helltaker/Chibi.xaml.cs
+++ Helltaker/Chibi.xaml.cs
@@ -1,65 +1,56 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Media;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Threading;
-using System.Windows.Forms;
 
-namespace Helltaker
-{
-    public partial class Chibi : Window
-    {
+namespace Helltaker {
+    public partial class Chibi : Window {
+
+        //이미지처리 관련
         Bitmap original;
         Bitmap[] frames = new Bitmap[12];
         ImageSource[] imgFrame = new ImageSource[12];
-
         [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool DeleteObject([In] IntPtr hObject);
-        public Chibi(string CharName)
-        {
+
+        public Chibi(string CharName) {
             InitializeComponent();
-            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+
+            // 이미지 가져오기
             original = System.Drawing.Image.FromFile(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/Resources/Chibi/" + CharName + ".png") as Bitmap;
-            
-            for(int i=0; i<12; i++)
-            {
-                frames[i] = new Bitmap(100,100);
-                using(Graphics g = Graphics.FromImage(frames[i]))
-                {
+
+            // 이미지 12등분으로 자르기.
+            for (int i = 0; i < 12; i++) {
+                frames[i] = new Bitmap(100, 100);
+                using (Graphics g = Graphics.FromImage(frames[i])) {
                     g.DrawImage(original, new System.Drawing.Rectangle(0, 0, 100, 100),
                         new System.Drawing.Rectangle(i * 100, 0, 100, 100),
                         GraphicsUnit.Pixel);
                 }
                 var handle = frames[i].GetHbitmap();
-                try
-                {
+                try {
                     imgFrame[i] = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
                 }
-                finally
-                {
+                finally {
                     DeleteObject(handle);
                 }
             }
+
+            // 스티커 움직임 갱신용 타이머 (주기가 짧을수록 싱크가 잘 맞음)
             System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(0.01);
             timer.Tick += NextFrame;
             timer.Start();
         }
 
-        private void NextFrame(object sender, EventArgs e)
-        {
-            Sticker.Source = imgFrame[MainWindow.syncro];
-        }
-
-        private void DragForm(object sender, MouseButtonEventArgs e) { this.DragMove(); e.Handled = true; }
-        private void ExitForm(object sender, MouseButtonEventArgs e) { Chibi.GetWindow(this).Close(); }
+        private void NextFrame(object sender, EventArgs e) { Sticker.Source = imgFrame[MainWindow.syncro]; } // 프레임갱신
+        private void DragForm(object sender, MouseButtonEventArgs e) { this.DragMove(); e.Handled = true; } // 폼 이동
+        private void ExitForm(object sender, MouseButtonEventArgs e) { Chibi.GetWindow(this).Close(); } // 더블클릭시 폼 삭제
     }
 }
--- Helltaker/MainWindow.xaml.cs
+++ Helltaker/MainWindow.xaml.cs
@@ -1,45 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
-using System.Media;
 
-namespace Helltaker
-{
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
-
-
-    public partial class MainWindow : Window
-    {
+namespace Helltaker {
+    public partial class MainWindow : Window {
 
         //잡다한것들 선언부
-        System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
-        public string[] NameIndex = new string[] { 
-            "판데모니카", 
-            "모데우스", 
-            "케르베로스", 
-            "말리나", 
-            "즈드라다", 
-            "아자젤", 
-            "저스티스", 
-            "루시퍼", 
+        System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon(); // 오른쪽 하단 작은 아이콘 생성
+        public string[] NameIndex = new string[] { // 이름 배열
+            "판데모니카",
+            "모데우스",
+            "케르베로스",
+            "말리나",
+            "즈드라다",
+            "아자젤",
+            "저스티스",
+            "루시퍼",
             "저지먼트"
         };
-        public string[] TitleString = new string[] {
+        public string[] TitleString = new string[] { // 캐릭터 설명 배열
             "피곤한 악마, 판데모니카",
             "음란한 악마, 모데우스",
             "세쌍둥이 악마, 케르베로스",
@@ -50,79 +31,75 @@ namespace Helltaker
             "지옥의 CEO, 루시퍼",
             "고위 기소관, 저지먼트"
         };
-        public int index = 0;
-        public static int syncro = 7;
+        public int index = 0; // 이름&설명의 인덱스
+        public static int syncro = 7; // 스티커들 싱크로맞추기
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
 
+            // 스티커들 움직임 관련 (프레임카운팅)
             System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(0.04);
             timer.Tick += AddFrame;
             timer.Start();
-        }  
+        }
 
+        private void AddFrame(object sender, EventArgs e) { syncro = (syncro + 1) % 12; } // 프레임카운팅 함수
+
+        // 캐릭터 선택버튼 함수.
         private void Next_Clicked(object sender, MouseButtonEventArgs e) { index++; if (index == 9) index = 0; ReloadScreen(); }
         private void Previous_Clicked(object sender, MouseButtonEventArgs e) { index--; if (index == -1) index = 8; ReloadScreen(); }
-        public void ReloadScreen()
-        {
+
+        // 선택된 캐릭터를 출력하는 함수.
+        public void ReloadScreen() {
             object TitleNode = this.FindName("Title");
             object ImageNode = this.FindName("Image");
-
-            if (TitleNode is Label)
-            {
+            if (TitleNode is Label) {
                 Label title = TitleNode as Label;
                 title.Content = TitleString[index];
             }
-            if(ImageNode is Image)
-            {
+            if (ImageNode is Image) {
                 Image image = ImageNode as Image;
                 image.Source = new BitmapImage(new Uri(@"/Resources/Sprite/" + NameIndex[index] + ".png", UriKind.Relative));
             }
         }
 
-        private void Addchibi(object sender, MouseButtonEventArgs e)
-        {
-            Chibi window = new Chibi(NameIndex[index]);
-            window.Show();
-        }
+        
+        private void Addchibi(object sender, MouseButtonEventArgs e) { Chibi window = new Chibi(NameIndex[index]); window.Show(); } // 스티커 생성 함수.
 
-        private void DragForm(object sender, MouseButtonEventArgs e) { this.DragMove(); e.Handled = true; }
+        private void DragForm(object sender, MouseButtonEventArgs e) { this.DragMove(); e.Handled = true; } // 창 드래그 함수.
 
-        private void MinimizeTray(object sender, MouseButtonEventArgs e)
-        {
+        private void MinimizeTray(object sender, MouseButtonEventArgs e) { // 종료시 프로그램이 꺼지지 않고 최소화되게함.
             this.Hide();
             this.ShowInTaskbar = false;
-
+            
+            // 우클릭시 나오는 선택창 선언.
             System.Windows.Forms.ContextMenu menu = new System.Windows.Forms.ContextMenu();
             ni.Icon = Properties.Resources.icon;
             ni.Visible = true;
             ni.ContextMenu = menu;
             ni.Text = "헬-테이커";
 
+            // 선택창 0번줄
             System.Windows.Forms.MenuItem i0 = new System.Windows.Forms.MenuItem();
             menu.MenuItems.Add(i0);
             i0.Index = 0;
             i0.Text = "만든사람 / 사용법";
-            i0.Click += delegate (object s, EventArgs e1) { System.Diagnostics.Process.Start("https://github.com/koder0205"); };
+            i0.Click += delegate (object s, EventArgs e1) { System.Diagnostics.Process.Start("https://github.com/Koder0205/Helltaker-Widjet"); };
 
+            // 선택창 1번줄
             System.Windows.Forms.MenuItem i1 = new System.Windows.Forms.MenuItem();
             menu.MenuItems.Add(i1);
             i1.Index = 1;
             i1.Text = "캐릭터생성창 열기";
             i1.Click += delegate (object s, EventArgs e1) { this.Show(); this.WindowState = WindowState.Normal; ni.Visible = false; this.ShowInTaskbar = true; };
 
+            // 선택창 2번줄
             System.Windows.Forms.MenuItem i2 = new System.Windows.Forms.MenuItem();
             menu.MenuItems.Add(i2);
             i2.Index = 2;
             i2.Text = "프로그램 종료하기";
             i2.Click += delegate (object s, EventArgs e1) { System.Windows.Application.Current.Shutdown(); ni.Dispose(); };
-        }
-
-        private void AddFrame(object sender, EventArgs e)
-        {
-            syncro = (syncro + 1) % 12;
         }
     }
 }
